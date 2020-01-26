047fdc3413eef3d5c13d96084f0932e72f848d0e
Napont Kitiwiriyakul <mrnonaki@gmail.com>
2020-01-26 13:09:37 +0700

    Update cpacketstream.cpp

--- src/cpacketstream.cpp
+++ src/cpacketstream.cpp
@@ -56,32 +56,32 @@ bool CPacketStream::Open(const CDvHeaderPacket &DvHeader, CClient *client)
         m_LastPacketTime.Now();
             switch (DvHeader.GetRpt2Module())
             {
-                case 'A':
-                case 'B':
-                case 'C':
-                case 'D':
-                case 'E':
-                //case 'F':
-                //case 'G':
-                //case 'H':
-                //case 'I':
-                //case 'J':
-                //case 'K':
-                //case 'L':
-                //case 'M':
-                case 'N':
-                //case 'O':
-                //case 'P':
-                //case 'Q':
-                //case 'R':
-                //case 'S':
-                case 'T':
-                //case 'U':
-                //case 'V':
-                //case 'W':
-                //case 'X':
-                case 'Y':
-                //case 'Z':
+                case A:
+                case B:
+                case C:
+                case D:
+                case E:
+                //case F:
+                //case G:
+                //case H:
+                //case I:
+                //case J:
+                //case K:
+                //case L:
+                //case M:
+                case N:
+                //case O:
+                //case P:
+                //case Q:
+                //case R:
+                //case S:
+                case T:
+                //case U:
+                //case V:
+                //case W:
+                //case X:
+                case Y:
+                //case Z:
                     m_CodecStream = g_Transcoder.GetStream(this, client->GetCodec());
                     break;
                 default:
